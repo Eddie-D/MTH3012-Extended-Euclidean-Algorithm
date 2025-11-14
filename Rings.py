99196fea63a498b0196a9dab38231cf8ea001113
eddiedoogan <eddiedoogan03@hotmail.co.uk>
2025-11-14 15:35:20 +0000

    Finish euclidean algorithm on zp and q

--- Rings.py
+++ Rings.py
@@ -1,4 +1,7 @@
-class Zn :
+from math import gcd
+import math
+
+class Zp :
     def __init__ (self, n, value) :
         self.n = n
         self.value = int(value) % n
@@ -8,15 +11,55 @@ class Zn :
     
     def __add__ (self, other) :
         self.checkArithMatch(other)
-        return Zn(self.n, self.value + other.value)
+        return Zp(self.n, self.value + other.value)
     
     def __sub__ (self, other) :
         self.checkArithMatch(other)
-        return Zn(self.n, self.value - other.value)
+        return Zp(self.n, self.value - other.value)
     
     def __mul__ (self, other) :
         self.checkArithMatch(other)
-        return Zn(self.n, self.value * other.value)
+        return Zp(self.n, self.value * other.value)
     
     def __str__ (self) :
-        return str(self.value)
+        return f"[{self.value}]_{{{self.n}}}"
+    
+    def __truediv__(self, other) :
+        # Calculate the inverse by fermats little theorem
+        self.checkArithMatch(other)
+        for i in range(self.n - 2) :
+            self *= other
+        return self
+
+    def __eq__(self, other):
+        # Don't check arithmatch as we instead use n to check if values are equal
+        return self.value == other.value and self.n == other.n
+
+class Frac :
+    def __init__ (self, num, den=1) :
+        if den == 0 : raise Exception("Fraction with denominator of zero")
+        # Re-write the fraction as an irreducible
+        # Ensure den is non-negative (solves issue of -1/-1 instead of 1/1)
+        factor = (1 if den > 0 else -1) * gcd(num, den)
+        self.num = int(num / factor)
+        self.den = int(den / factor)
+    
+    def __add__ (self, other) :
+        return Frac(self.num * other.den + other.num * self.den, self.den * other.den)
+
+    def __sub__(self, other) :
+        return Frac(self.num * other.den - other.num * self.den, self.den * other.den)
+
+    def __mul__(self, other) :
+        return Frac(self.num * other.num, self.den * other.den)
+
+    def __truediv__(self, other) :
+        return Frac(self.num * other.den, self.den * other.num)
+    
+    def __str__(self) :
+        if self.den == 1 : return str(self.num)
+        else: return f"\\frac{{{self.num}}}{{{self.den}}}"
+
+    def __eq__(self, other) :
+        # Can check numerator and denominator equal as we store fractions uniquely as irreducibles with negatives ontop
+        return self.num == other.num and self.den == other.den
--- polynomial-long-division.py
+++ polynomial-long-division.py
@@ -3,7 +3,7 @@
 # We create classes for the different types of rings
 import numpy as np
 from functools import partial
-from rings import Zn
+from rings import Zp, Frac
 
 # We impose the following qualities on our ring objects:
 # Implementation of *, + and -
@@ -15,28 +15,51 @@ def getUserMatrix(n) :
     rows = []
     for i in range(n) :
         print("Please enter the values for row", i)
-        rows.append([int(x) for x in input().split(' ')])
+        tillValid(lambda: rows.append([int(x) for x in input().split(' ')]), "Please enter the integer values separated by spaces")
     return np.matrix(rows)
 
+def tillValid(GetElement, repeatMessage) :
+    valid = False
+    while not valid :
+        valid = True
+        try :
+            e = GetElement()
+        except KeyboardInterrupt:
+            # We want keyboard interrupt to still exit the script
+            raise KeyboardInterrupt
+        except:
+            # If we have invalid input in GetElement
+           print(repeatMessage)
+           valid = False
+    return e
+
+# Setup ring-specific variables
 fromConsole = None
+unit = None
+zero = None
+
 while(not fromConsole) :
-    print("Please choose a ring to operate in (Z, Z/n, Mn)")
+    print("Please choose a field to operate in (Q, Z/p, Mn)")
     choice = input()
     match choice.lower() :
-        case "z/n" | "zn":
+        case "z/p" | "zp" | "z":
             print("Please choose n for Z/n")
             n = int(input())
             # Define the ring specifically for our N
-            fromConsole = lambda: Zn(n, int(input()))
-        
-        case "z" :
-            fromConsole = lambda: int(input())
+            fromConsole = lambda: tillValid(lambda: Zp(n, int(input())), "Please choose an integer mod p") 
+            unit = Zp(n,1)
+            zero = Zp(n,0)
+
+        case "q" :
+            fromConsole = lambda: tillValid(lambda: Frac(*[int(x) for x in input().split("/")]), "Enter a fraction in the form X/Y for integers X and Y")
+            unit = Frac(1,1)
+            zero = Frac(0,1)
 
         case "mn" | "m":
             print("Please choose n")
-            n = int(input())
+            n = tillValid(lambda: int(input()), "Enter an integer for the size of the square matrix")
             fromConsole = lambda: getUserMatrix(n)
-
+            unit = np.identity(n)
 
 print("Please enter the degree of the dividend")
 degree = int(input())
@@ -52,23 +75,78 @@ for i in range(degree,-1,-1) :
     print("Please enter the coefficients for X^", i)
     divisor.append(fromConsole())
 
+# Performs polynomial long division assuming the underlying ring is a field.
 def polyLongDivide(dividend, divisor) :
     remainder = list.copy(dividend)
     quotient = []
 
     def destroyLeading() :
-        mult = remainder[0]
+        mult = remainder[0] / divisor[0]
+        # print("Mult:" + str(mult) + "  Remainder: " + str(remainder[0]) + "  Divisor: " + str(divisor[0]))
         for i in range(len(divisor)) :
             remainder[i] = remainder[i] - (divisor[i] * mult)
-        remainder.pop(0)
+        remainder.pop(0) # We can remove the first element as it will have cancelled
         quotient.append(mult)
 
     while(len(divisor) <= len(remainder)) :
         destroyLeading()
+
     return (quotient, remainder)
 
-while (len(divisor) > 1) :
-    dividend, divisor = polyLongDivide(dividend, divisor)
+def strTerm(coeff, pow) :
+    if coeff == zero :
+        return ""
+    elif pow >= 1:
+        strCo = str(coeff) if coeff != unit else ""
+        strX = f"X^{{{pow}}}" if pow > 1 else "X"
+        return strCo + strX
+    else :
+        return str(coeff)
 
-print("The quotient is: ", [str(x) for x in dividend])
-print("The remainder is: ", [str(x) for x in divisor])
+def strPoly(p) :
+    if all(c == zero for c in p) :
+        # Return constant zero if all coefficients are zero
+        return str(zero)
+    strP = [strTerm(p[i], len(p)-i-1) for i in range (0, len(p))]
+    return " + ".join(filter(lambda s: s != "", strP))
+
+md = open("ExtendedEuclideanOutput.md", "w")
+md.write("# Extended Euclidean Algorithm \n")
+md.write(f"### We find the gcd of ${strPoly(dividend)}$ and ${strPoly(divisor)}$\n\n")
+results = []
+while (len(divisor) > 0) :
+    # Create the divisors monic associate
+    # inverse = (unit / divisor[0])
+    # divisor = [inverse * d for d in divisor]
+
+    quotient, remainder = polyLongDivide(dividend, divisor)
+    s = f"1. ${strPoly(dividend)} = ({strPoly(quotient)})({strPoly(divisor)}) + ({strPoly(remainder)})$"
+    results.append([dividend, quotient, divisor, remainder])
+    print(s)
+    md.write(s + "\n\n")
+
+    dividend = divisor
+    divisor = remainder
+
+    # Remove any leading zeroes
+    while len(divisor) > 0 and divisor[0] == zero :
+        divisor.pop(0)
+
+monicMult = unit/(dividend[0])
+gcd = [monicMult * d for d in dividend]
+md.write(f"Overall the gcd is ${strPoly(dividend)}$ or as its monic associate, ${strPoly(gcd)}$\n\n")
+
+md.write(f"### We use the Extended Euclidean algorithm to find coefficients $\mu$, $\lambda$ s.t. $\mu ({strPoly(dividend)}) + \lambda ({strPoly(divisor)}) = {strPoly(gcd)}$\n\n")
+# print("$" + "$\n\n$".join(["$,$".join([strPoly(p) for p in r]) for r in results]) + "$")
+# md.write("\n\n\n\n$" + "$\n\n$".join(["$,&nbsp;$".join([strPoly(p) for p in r]) for r in results]) + "$")
+
+mu = monicMult
+lam = unit
+# Remove result with zero remainder
+results.pop()
+results.reverse()
+for [dividend, quotient, divisor, remainder] in results :
+    s = f"${strPoly(remainder)} = {strPoly(dividend)} - ({strPoly(quotient)})({strPoly(divisor)})$"
+    md.write(f"{s}\n\n")
+
+remainder = divided - (quotient * divisor)
